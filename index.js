6e402e3e49b48d0cd2726e44bf8b9d182f141b26
roy <kimathiwaweru@gmail.com>
2022-11-23 15:45:10 +0300

    updated template.html, created index.js file

---
+++ index.js
@@ -0,0 +1,5 @@
+let root = document.getElementByID(`root`);
+let header = document.getElementsByTagName(`header`);
+let sidebar = document.getElementByID(`sideBar`);
+let mainDiv = document.getElementByID(`mainDiv`);
+let footer = document.getElementsByID(`mainFooter`);
---
+++ myScripts.js
@@ -0,0 +1,31 @@
+//getElementByID()
+//getElementByClassName()
+//getElementByTagName()
+
+//createElement()
+//append()
+//innerHTML
+//textContent
+
+
+// this gets the root element from home.html
+//let root = document.getElementById("root");
+//this creates a button element
+// let btn = document.createElement("button");
+// function btnEvents(color){
+//     btn.style.backgroundColor = color;
+// }
+// btn.addEventListener('mousemove', function()){
+//     btn.style.backgroundColor = color;
+//}
+// //this adds textcontent to our btn
+// btn.textContent = "submit";
+// //this append js element btn to the dom
+// root.appendChild(btn);
+// //this creates a paragraph element
+// let p = document.createElement("p");
+// //adds textcontent
+// p.textContent = "Hey This is added from the dom";
+// //this appends js element p to the dom
+// root.appendChild(p);
+
